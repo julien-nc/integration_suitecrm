d7c19bafe499d4c55d14f620af332c106fe9d150
Julien Veyssier <eneiluj@posteo.net>
2020-10-15 18:29:04 +0200

    add color and hide modules in webpack config
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- webpack.js
+++ webpack.js
@@ -5,10 +5,15 @@ const buildMode = process.env.NODE_ENV
 const isDev = buildMode === 'development'
 webpackConfig.devtool = isDev ? 'cheap-source-map' : 'source-map'
 
+webpackConfig.stats = {
+    colors: true,
+    excludeModules: true,
+}
+
 webpackConfig.entry = {
-	personalSettings: { import: path.join(__dirname, 'src', 'personalSettings.js'), filename: 'integration_suitecrm-personalSettings.js' },
-	adminSettings: { import: path.join(__dirname, 'src', 'adminSettings.js'), filename: 'integration_suitecrm-adminSettings.js' },
-	dashboard: { import: path.join(__dirname, 'src', 'dashboard.js'), filename: 'integration_suitecrm-dashboard.js' },
+    personalSettings: { import: path.join(__dirname, 'src', 'personalSettings.js'), filename: 'integration_suitecrm-personalSettings.js' },
+    adminSettings: { import: path.join(__dirname, 'src', 'adminSettings.js'), filename: 'integration_suitecrm-adminSettings.js' },
+    dashboard: { import: path.join(__dirname, 'src', 'dashboard.js'), filename: 'integration_suitecrm-dashboard.js' },
 }
 
 module.exports = webpackConfig
